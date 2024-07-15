84a139b08f9afc9475998659a318f4dceab14dc7
francescokrum <francescopk19@gmail.com>
2024-07-15 19:33:42 -0300

    (refactor): alterando rotas login

--- src/app/_components/form-login/form-login.component.ts
+++ src/app/_components/form-login/form-login.component.ts
@@ -1,5 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
+import { Login } from '../../auth/login';
+import { LoginService } from '../../auth/login-service.service';
+import { NgForm } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
 
 @Component({
   selector: 'app-form-login',
@@ -8,11 +13,43 @@ import { Router } from '@angular/router';
 })
 export class FormLoginComponent {
 
-  constructor(private router: Router) {}
+  login: Login = new Login()
 
-  onSubmit(event: Event) {
-    event.preventDefault();
+  router = inject(Router);
+  loginService = inject(LoginService)
 
-    this.router.navigate(['/paginaInicialAdmin']);
+  constructor(private snackBar: MatSnackBar) {
+    this.loginService.removerToken();
   }
+
+  logar() {
+    this.loginService.logar(this.login).subscribe({
+      next: token => {
+        if(token){
+          this.loginService.addToken(token)
+          console.log(token)
+          if(this.loginService.hasPermission("ADMIN")){
+            this.router.navigate(['/admin'])
+          }
+          else if(this.loginService.hasPermission("CLIENTE")){
+            this.router.navigate(['/cliente'])
+          }
+          else if(this.loginService.hasPermission("DEV")){
+            this.router.navigate(['/dev'])
+          }
+          this.snackBar.open('Usuário autenticado com sucesso!', 'Fechar', {
+            duration: 3000,
+            panelClass: ['green-snackbar']
+          });
+        }
+        else {
+          alert('não veio token!')
+        }
+      },
+      error: erro => {
+        alert('Login ou Senha incorretos!');
+      }
+    })
+  }
+
 }
